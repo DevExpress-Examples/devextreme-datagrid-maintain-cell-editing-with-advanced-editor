6985fa13f45a605367a340a7b5de1705adfae8a6
AssylbekDanyshbek <danyshbek1996@gmail.com>
2024-04-01 15:16:06 +0500

    angular sample take 2

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,20 +1,73 @@
-import { Component } from '@angular/core';
-import { ClickEvent } from 'devextreme/ui/button';
+import {
+  Component, ElementRef, OnInit, ViewChild,
+} from '@angular/core';
+import DataGrid from 'devextreme/ui/data_grid';
+import Popup from 'devextreme/ui/popup';
+import { DxDataGridComponent, DxPopupComponent } from 'devextreme-angular';
+import {
+  employees, states, tasks, titlesPrefix,
+} from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Angular';
+export class AppComponent implements OnInit {
+  @ViewChild('targetDataGrid', { static: false }) dataGrid?: DxDataGridComponent;
 
-  counter = 0;
+  @ViewChild('targetPopup', { static: false }) popup?: DxPopupComponent;
 
-  buttonText = 'Click count: 0';
+  dropdownOptions: any;
 
-  onClick(e: ClickEvent): void {
-    this.counter++;
-    this.buttonText = `Click count: ${this.counter}`;
+  tasks = tasks;
+
+  employees = employees;
+
+  titlePrefix = titlesPrefix;
+
+  states = states;
+
+  private readonly overlayClass = 'dx-overlay-wrapper';
+
+  private readonly blockClickClass = 'dx-dropdowneditor-overlay';
+
+  constructor(private readonly elementRef: ElementRef) {
+    this.dropdownOptions = {
+      width: '500px',
+    };
+  }
+
+  ngOnInit(): void {
+    const observer = new MutationObserver(() => {
+      this.handleMutation();
+    });
+    observer.observe(this.elementRef.nativeElement.parentElement, {
+      attributes: false,
+      childList: true,
+      subtree: false,
+    });
+  }
+
+  private handleMutation(): void {
+    const overlays = document.querySelectorAll(`.${this.overlayClass}`);
+    overlays.forEach((overlay) => {
+      overlay.classList.add(this.blockClickClass);
+    });
+  }
+
+  onSelectionChanged(selectedRowKeys: any, cellInfo: any, component: any): void {
+    cellInfo.setValue(selectedRowKeys[0]);
+    if (selectedRowKeys.length > 0) {
+      component.close();
+    }
+  }
+
+  searchValueChanged(value: string): void {
+    this.dataGrid?.instance.searchByText(value);
+  }
+
+  openPopup(): void {
+    this.popup?.instance.show().catch(() => {});
   }
 }
