d2b464ca4c1e8ef0bc63943984230a6f6f2bc5c2
AssylbekDanyshbek <danyshbek1996@gmail.com>
2024-04-09 15:52:05 +0500

    react sample

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,18 +1,51 @@
-import React, { useCallback, useState } from 'react';
 import './App.css';
-import 'devextreme/dist/css/dx.material.blue.light.compact.css';
-import Button from 'devextreme-react/button';
+import 'devextreme/dist/css/dx.light.css';
+import React from 'react';
+import DataSource from 'devextreme/data/data_source';
+import ArrayStore from 'devextreme/data/array_store';
+import { DataGrid } from 'devextreme-react';
+import {
+  Column, Editing, Lookup, Paging,
+} from 'devextreme-react/data-grid';
+import { employees, tasks } from './data';
+import useOverlayObserver from './OverlayObserver';
+import DropDownBoxComponent from './DropDownBoxComponent';
 
-function App(): JSX.Element {
-  var [count, setCount] = useState<number>(0);
-  const clickHandler = useCallback(() => {
-    setCount((prev) => prev + 1);
-  }, [setCount]);
+const dataSource = new DataSource({
+  store: new ArrayStore({
+    data: tasks,
+    key: 'ID',
+  }),
+});
+
+const App = (): JSX.Element => {
+  useOverlayObserver();
+
   return (
-    <div className="main">
-      <Button text={`Click count: ${count}`} onClick={clickHandler} />
-    </div>
+    <React.Fragment>
+      <DataGrid
+        dataSource={dataSource}
+        showBorders={true}>
+        <Paging
+          enabled={true}
+          pageSize={15}>
+        </Paging>
+        <Editing
+          mode="cell"
+          allowUpdating={true}
+        ></Editing>
+        <Column
+          dataField='AssigneeID'
+          editCellComponent={DropDownBoxComponent}>
+          <Lookup
+            dataSource={employees}
+            valueExpr="ID"
+            displayExpr="FullName"></Lookup>
+        </Column>
+        <Column dataField='Task'></Column>
+      </DataGrid>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
