e591514518f3af706e43a2dbc8766294c89fc3e0
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2025-12-13 10:54:28 +0200

    Migrate to eslint-config-devextreme 1.1.8

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,78 +1,20 @@
-import {
-  Component, ElementRef, OnInit, ViewChild,
-} from '@angular/core';
-import DropDownBox from 'devextreme/ui/drop_down_box';
-import { DxDataGridComponent, DxPopupComponent } from 'devextreme-angular';
-import { ColumnEditCellTemplateData } from 'devextreme/ui/data_grid';
-import {
-  Employee,
-  employees, states, tasks, titlesPrefix,
-} from './app.service';
+import { Component } from '@angular/core';
+import { ClickEvent } from 'devextreme/ui/button';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  @ViewChild('targetDataGrid', { static: false }) dataGrid?: DxDataGridComponent;
+export class AppComponent {
+  title = 'Angular';
 
-  @ViewChild('targetPopup', { static: false }) popup?: DxPopupComponent;
+  counter = 0;
 
-  dropdownOptions: any;
+  buttonText = 'Click count: 0';
 
-  tasks = tasks;
-
-  employees = employees;
-
-  titlePrefix = titlesPrefix;
-
-  states = states;
-
-  popupVisible: boolean;
-
-  private readonly overlayClass = 'dx-overlay-wrapper';
-
-  private readonly blockClickClass = 'dx-dropdowneditor-overlay';
-
-  constructor(private readonly elementRef: ElementRef) {
-    this.dropdownOptions = {
-      width: '500px',
-    };
-    this.popupVisible = false;
-  }
-
-  ngOnInit(): void {
-    const observer = new MutationObserver(() => {
-      this.handleMutation();
-    });
-    observer.observe(this.elementRef.nativeElement.parentElement, {
-      attributes: false,
-      childList: true,
-      subtree: false,
-    });
-  }
-
-  private handleMutation(): void {
-    const overlays = document.querySelectorAll(`.${this.overlayClass}`);
-    overlays.forEach((overlay) => {
-      overlay.classList.add(this.blockClickClass);
-    });
-  }
-
-  onSelectionChanged(selectedRowKeys: Employee[], cellInfo: ColumnEditCellTemplateData, component: DropDownBox): void {
-    cellInfo.setValue(selectedRowKeys[0]);
-    if (selectedRowKeys.length > 0) {
-      component.close();
-      this.popupVisible = false;
-    }
-  }
-
-  searchValueChanged(value: string): void {
-    this.dataGrid?.instance.searchByText(value);
-  }
-
-  openPopup(): void {
-    this.popupVisible = true;
+  onClick(_e: ClickEvent): void {
+    this.counter++;
+    this.buttonText = `Click count: ${this.counter}`;
   }
 }
