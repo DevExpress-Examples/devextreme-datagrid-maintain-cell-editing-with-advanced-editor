fcf93d5854c7314f2158c40d46a58f028d6a8bf7
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-04-03 10:44:56 +0300

    Remove no-unused-vars

--- Vue/src/components/DropDownBoxComponent.vue
+++ Vue/src/components/DropDownBoxComponent.vue
@@ -15,7 +15,6 @@ const dataGridColumns = ['FullName', 'State', 'City'];
 
 const props = defineProps<{
   value: number,
-  // eslint-disable-next-line no-unused-vars
   onValueChanged(value:number): void,
   dataSource: Task[],
 }>();
