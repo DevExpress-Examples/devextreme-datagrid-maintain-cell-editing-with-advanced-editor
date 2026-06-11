68b90645dcb0d829ab4eb73679747cb52fd4b856
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-11 21:30:28 +0300

    Fix DevExtreme licensing import order

--- Vue/src/main.ts
+++ Vue/src/main.ts
@@ -1,14 +1,12 @@
-import config from 'devextreme/core/config';
-import { licenseKey } from './devextreme-license';
-
-config({ licenseKey });
-
 import { createApp } from 'vue';
+import config from 'devextreme/core/config';
 import App from './App.vue';
 import router from './router';
 import { overlayObserverDirective } from './overlayObserverDirective';
-
 import './assets/main.css';
+import { licenseKey } from './devextreme-license';
+
+config({ licenseKey });
 
 const app = createApp(App);
 
